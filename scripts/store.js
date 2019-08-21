a2d8bdfe6c826189690498a3cd7bb9f8feb75689
Marlon Agno <mdj.agno@gmail.com>
2019-08-21 11:10:13 -0400

    Started project structure and modules exercise

---
+++ scripts/store.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+const store = (function(){
+  const foo = 'bar';
+  let items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  };
+
+}() );
+
+console.log(store);
+console.log(foo);
