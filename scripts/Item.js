fd6520dad59e23dbbc2f48c15af80ee13bef49cb
Marlon Agno <mdj.agno@gmail.com>
2019-08-21 11:38:31 -0400

    step 1 namespacing

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,6 +1,25 @@
 'use strict';
 
-const Item = (function() {
-  return {};
+const Item = (function () {
+
+  const validateName = function(name) {
+    if (!name) {
+      throw new TypeError('Name does not exist');
+    }
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name,
+      checked: false
+    };
+  };
+
+  return {
+    validateName,
+    create,
+  };
+
 }() );
 
